76738dad2a17fe457b07ab49468ac5fff0237e1b
Andrea Mackie <anmack11@hotmail.com>
2019-07-06 04:12:55 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,9 +4,9 @@ var playlist = {
 }
 
 function updatePlaylist(playlist,artistName,songTitle) {
-playlist.artistName = songTitle 
+//playlist.artistName = songTitle 
  
- //Object.assign({}, playlist, {artistName:{songTitle}})
+ Object.assign({}, playlist, {artistName:{songTitle}})
  
  
  
