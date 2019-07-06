5c34e65a1761b6fea312ae13189e99280dd6ba1a
Andrea Mackie <anmack11@hotmail.com>
2019-07-06 03:55:30 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist = {
   
 }
 
-function updatePlaylist(playlist,artist1,songtitle) {
- playlist.artist1 = songtitle 
+function updatePlaylist(playlist,artistName,songTitle) {
+ playlist.artistName = songTitle 
 }
